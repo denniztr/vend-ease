5818567429fbefb098c15fb3d0f19b878ea721b9
denniztr <denniztr@gmail.com>
2024-02-23 12:59:58 +0300

    few fixes

--- app/dashboard/[branch]/equipments/[id]/page.tsx
+++ app/dashboard/[branch]/equipments/[id]/page.tsx
@@ -2,7 +2,6 @@ import getEquipmentByIndex from '@/app/actions/get-equipment';
 
 export default async function Home({ params }: { params: any }) {
   const equipment = await getEquipmentByIndex(params.branch, params.id);
-  console.log(equipment)
   return (
     <div>
       Оборудование:
