df254d3c5a16408a3156c0a6b5156f1c8f360270
denniztr <denniztr@gmail.com>
2024-02-23 20:13:16 +0300

    equipment's photos with carousel

--- app/dashboard/[branch]/equipments/[id]/page.tsx
+++ app/dashboard/[branch]/equipments/[id]/page.tsx
@@ -6,11 +6,24 @@ export default async function Home({ params }: { params: any }) {
   return (
     <div className="h-full flex flex-col gap-2">
       <div className="py-4 flex flex-col gap-1 text-md text-gray-600 font-semibold">
-        <p><span className="font-medium">Адрес: </span>{equipment.address}</p>
-        <p><span className="font-medium">Оборудование: </span>{equipment.equipment}</p>
-        <p><span className="font-medium">Серийный номер: </span>{equipment.serial_number}</p>
+        <p>
+          <span className="font-medium">Название точки: </span>
+          {equipment.label}
+        </p>
+        <p>
+          <span className="font-medium">Адрес: </span>
+          {equipment.address},
+        </p>
+        <p>
+          <span className="font-medium">Оборудование: </span>
+          {equipment.equipment}
+        </p>
+        <p>
+          <span className="font-medium">Серийный номер: </span>
+          {equipment.serial_number}
+        </p>
       </div>
-        <Body equipment={equipment} />
+      <Body equipment={equipment} />
     </div>
   );
 }
--- app/dashboard/[branch]/equipments/[id]/ui/body.tsx
+++ app/dashboard/[branch]/equipments/[id]/ui/body.tsx
@@ -1,6 +1,5 @@
-'use client';
-
 import { Branch } from '@/app/interface/branch';
+import PrimaryImage from '@/app/ui/image/image';
 
 
 interface BodyProps {
@@ -9,8 +8,13 @@ interface BodyProps {
 
 const Body: React.FC<BodyProps> = ({ equipment }) => {
   return (
-    <div className="w-[30rem] h-[30rem]">
-
+    <div className="min-h-full flex flex-col flex-nowrap">
+      <div className="flex flex-row gap-4">
+        <PrimaryImage />
+        <main className="w-full flex flex-col gap-1">
+          <div>main info</div>
+        </main>
+      </div>
     </div>
   );
 };
