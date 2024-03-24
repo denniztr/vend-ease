a65289db7031283c1ee95fbf92c39e879a268b07
denniztr <denniztr@gmail.com>
2024-03-24 16:02:47 +0300

    heading, add new item button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import { data } from '@/app/mockdata/data';
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col gap-4">
+    <div className="h-full flex flex-col gap-2">
       <div className="h-1/2 grid grid-cols-3 gap-2">
         <TodoCard />
         <div className="flex flex-col gap-4">
