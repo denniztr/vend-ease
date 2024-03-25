1adbfd8e8ee2b9cb6737f261b1b8dd7ad09cc7a3
denniztr <denniztr@gmail.com>
2024-03-25 14:53:39 +0300

    todo db, page init

--- app/ui/card/ui/heading.tsx
+++ app/ui/card/ui/heading.tsx
@@ -1,3 +1,5 @@
+
+import Link from 'next/link';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import LaunchIcon from '@mui/icons-material/Launch';
@@ -17,7 +19,9 @@ const Heading: React.FC<HeadingProps> = ({ children }) => {
         <h3 className="text-xl font-semibold">{children}</h3>
       </div>
       <div>
-        <AddBoxIcon className="w-6 h-6 cursor-pointer" />
+        <Link href='/dashboard/add'>
+          <AddBoxIcon className="w-6 h-6 cursor-pointer" />
+        </Link>
         <LaunchIcon className="ml-1 w-6 h-6 text-gray-500/50 cursor-pointer transition duration-500 hover:text-gray-600" />
       </div>
     </div>
